5c038c24dc728f11c99c9ce81514b7687203d7d0
Laura Olszewska <laura.olszewska.0506@gmail.com>
2025-05-18 13:05:17 +0200

    Dodano wyświeltanie szacowango czasu ukończenia zadania oraz opóźnienia

--- ProjectCard.java
+++ ProjectCard.java
@@ -1,13 +1,15 @@
-import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import javax.swing.*;
 
 public class ProjectCard extends JPanel {
     public ProjectCard(Project project, Runnable onClick) {
         setLayout(new BorderLayout());
         setBackground(Color.WHITE);
-        setMaximumSize(new Dimension(Integer.MAX_VALUE, 120));
+        setMaximumSize(new Dimension(Integer.MAX_VALUE, 150));
         setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 
         // === Kolorowy border z lewej ===
@@ -33,10 +35,33 @@ public class ProjectCard extends JPanel {
 
         add(infoPanel, BorderLayout.CENTER);
 
+        //== Panel z datami ==
+        JPanel datesPanel = new JPanel(new GridLayout(2,2,5,5));
+        datesPanel.setOpaque(false);
+        datesPanel.setBorder(BorderFactory.createEmptyBorder(5,0,0,0));
+
+        //Termin koncowy
+        datesPanel.add(new JLabel("Termin: "));
+        datesPanel.add(new JLabel(formatDate(project.deadline)));
+
+        //Szacowane zakonczenie
+        datesPanel.add(new JLabel("Szacowany czas: "));
+        datesPanel.add(new JLabel(project.predictDate()));
+
+        infoPanel.add(datesPanel);
+
+        //==Opoznienie==
+
+        JLabel delayLabel = new JLabel("Opóźnienie: "+ Math.max(project.delay,0) + " dni");
+        delayLabel.setFont(new Font("Arial", Font.BOLD,12));
+        delayLabel.setForeground(project.delay > 0 ? Color.RED : Color.GREEN);
+        infoPanel.add(delayLabel);
+
         // === Pasek postępu ===
         JProgressBar progressBar = new JProgressBar(0, 100);
         progressBar.setValue(project.progress());
         progressBar.setStringPainted(true);
+        progressBar.setPreferredSize(new Dimension(100,20));
         add(progressBar, BorderLayout.EAST);
 
         // === Obsługa kliknięcia ===
@@ -46,5 +71,8 @@ public class ProjectCard extends JPanel {
                 onClick.run();
             }
         });
+    }
+    private String formatDate(long epochDay){
+        return LocalDate.ofEpochDay(epochDay).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 }
